3a31b105793ede5f26034c44d841ea033e08ff64
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2026-02-25 00:59:09 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dsc-prometheus-grafana
+
+## dsc
+
+- <https://codeberg.org/DNS-OARC/dsc/src/branch/main>
+- <https://codeberg.org/DNS-OARC/dsc-datatool>
+- <https://dev.dns-oarc.net/packages/>
+
+## Setup
+
+### dsc-datatool
+
+```bash
+python3 -m venv venv --system-site-packages
+. venv/bin/activate
+pip install -r requirements.txt
+pip install -e . --no-deps
+```
---
+++ dsc-promexporter/dsc-promexporter.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -eu
+
+SERVER=
+NODE=
+
+DSC_DIR=/var/lib/dsc
+OUT_DIR=/var/lib/prometheus/node-exporter
+OUT_FILE=$OUT_DIR/dsc.prom
+LATEST=$(ls -1 $DSC_DIR/*.xml | tail -n 1)
+TMP=$(mktemp "$OUT_DIR/dsc.prom.XXXXXX")
+DATASETS="\
+qtype,\
+rcode,\
+opcode,\
+rcode_vs_replylen,\
+client_subnet,\
+qtype_vs_qnamelen,\
+qtype_vs_tld,\
+certain_qnames_vs_qtype,\
+client_subnet2,\
+client_addr_vs_rcode,\
+chaos_types_and_names,\
+country_code,\
+asn_all,\
+idn_qname,\
+edns_version,\
+edns_bufsiz,\
+do_bit,\
+rd_bit,\
+idn_vs_tld,\
+ipv6_rsn_abusers,\
+transport_vs_qtype,\
+client_port_range,\
+second_ld_vs_rcode,\
+third_ld_vs_rcode,\
+direction_vs_ipproto,\
+dns_ip_version_vs_qtype,\
+response_time,\
+label_count,\
+qr_aa_bits"
+
+cleanup() {
+  rm -f "$TMP"
+}
+trap cleanup EXIT
+
+dsc-datatool \
+  -x "$LATEST" \
+  -s "$SERVER" \
+  -n "$NODE" \
+  --dataset $DATASETS \
+  -o ";Prometheus;timestamp=hide;prefix=dsc_" \
+  > "$TMP"
+
+install -m 0644 "$TMP" "$OUT_FILE"
+
+find "$DSC_DIR" -name '*.xml' -mmin +60 -delete
